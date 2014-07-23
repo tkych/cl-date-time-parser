c3deb99c3198a822386573fb222a6df12f878c92
Takaya OCHIAI <tkych.repl@gmail.com>
2014-07-23 20:24:01 +0900

    Remove eval-tests.

--- date-time-parser.lisp
+++ date-time-parser.lisp
@@ -1,6 +1,4 @@
-;;;; Last modified: 2014-05-18 14:40:06 tkych
-
-;; cl-date-time-parser/date-time-parser.lisp
+;;;; cl-date-time-parser/date-time-parser.lisp
 
 ;; Copyright (c) 2013 Takaya OCHIAI <tkych.repl@gmail.com>
 ;; This software is released under the MIT License.
@@ -21,46 +19,6 @@
   (:export #:parse-date-time))
 
 (in-package #:cl-date-time-parser)
-
-
-;;--------------------------------------------------------------------
-;; Eval-Test for CL-Date-Time-Parser
-;;--------------------------------------------------------------------
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defparameter *features-tmp* *features*)
-
-  ;; when release, the following line should be comment in.
-  (setf *features* (remove :et *features*))
-
-  ;; when release, the following two lines should be comment out.
-  ;; (ql:quickload '(:cl-ppcre :split-sequence :anaphora :local-time :parse-float))
-  ;; (pushnew :et *features*)
-  )
-
-#+et
-(eval-when (:compile-toplevel :load-toplevel :execute)
-
-  (defmacro =>? (form want &optional test)
-    "Check whether FORM is evaluated to WANT by TEST (default is `equal`).
-If first element of WANT is `:values`, then check mutiple values."
-    (if (and (listp want) (eq :values (first want)))
-        `(assert (funcall ,(if test test ''equal)
-                          (multiple-value-list ,form)
-                          (list ,@(rest want))))
-        `(assert (funcall ,(if test test ''equal)
-                          ,form ,want))))
-
-  (defmacro =>t? (form)
-    "Check whether FORM is evaluated to T."
-    `(=>? ,form t))
-
-  (defmacro =>nil? (form)
-    "Check whether FORM is evaluated to NIL."
-    `(=>? ,form nil))
-
-  ) ;end of #+et
-
 
 ;;--------------------------------------------------------------------
 ;; Special Variables
@@ -224,25 +182,8 @@ If first element of WANT is `:values`, then check mutiple values."
            :do (when (equal tz-abbrev (local-time::subzone-abbrev sub))
                  (return (- (local-time::subzone-offset sub)))))))))
 
-#+et (=>? (get-offset "JST") (* -9 60 60))
-#+et (=>? (get-offset "GMT") 0)
-#+et (=>? (get-offset "MDT") (* -6 60 60))
-
-
 (defun year-to-ut (year)
   (encode-universal-time 0 0 0 1 1 year 0))
-
-#+et (=>? (year-to-ut 1900) 0)
-#+et (=>? (year-to-ut 1901) (* 365 24 60 60))
-#+et (=>? (year-to-ut 1902) (+ (year-to-ut 1901) (* 365 24 60 60)))
-#+et (=>? (year-to-ut 1905) (+ (year-to-ut 1904) (* 366 24 60 60)))
-#+et (=>? (year-to-ut 2000)
-          (local-time:timestamp-to-universal
-           (local-time:parse-timestring "2000-01-01T00:00:00Z")))
-#+et (=>? (year-to-ut 2013)
-          (local-time:timestamp-to-universal
-           (local-time:parse-timestring "2013-01-01T00:00:00Z")))
-
 
 (defun leap-year-p (year)
   "Return T if YEAR is a leap year, otherwise NIL.
@@ -251,11 +192,6 @@ c.f. RFC3339, (Appendix C. Leap Years)"
   (and (zerop (mod year 4))
        (or (not (zerop (mod year 100)))
            (zerop (mod year 400)))))
-
-#+et (=>nil? (leap-year-p 1999))
-#+et (=>t?   (leap-year-p 2000))
-#+et (=>nil? (leap-year-p 2001))
-
 
 (defun month-to-ut (month leap-year?)
   (etypecase month
@@ -266,25 +202,6 @@ c.f. RFC3339, (Appendix C. Leap Years)"
                         *month-vec-in-leap-year*
                         *month-vec-in-normal-year*)
                     month))))
-
-#+et (=>t? (and (every (lambda (m) (= (month-to-ut m t)
-                                      (month-to-ut m nil)))
-                       '("Jan" "January" 1))
-                (every (lambda (m) (= (month-to-ut m t)
-                                      (month-to-ut m nil)))
-                       '("Feb" "February" 2))
-                (every (lambda (m) (= (* 24 60 60)
-                                      (- (month-to-ut m t)
-                                         (month-to-ut m nil))))
-                       '("Mar" "March" 3))
-                (every (lambda (m) (= (* 24 60 60)
-                                      (- (month-to-ut m t)
-                                         (month-to-ut m nil))))
-                       '("Apr" "April" 4))
-                (every (lambda (m) (= (* 24 60 60)
-                                      (- (month-to-ut m t)
-                                         (month-to-ut m nil))))
-                       '("Dec" "December" 12))))
 
 
 (defun parse-rfc822-genus (date-time-string)
@@ -396,89 +313,6 @@ Reference:
       (incf universal-time (month-to-ut month leap-year?)))
 
     (values universal-time fraction)))
-
-
-#+et (flet ((rfc822 (x) (parse-rfc822-genus x))
-            (enc (&rest args) (apply #'encode-universal-time args)))
-
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00 GMT")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00 gmt")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00 Z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00Z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00 z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-
-       (=>? (rfc822 "Sat, 02 Mar 2013 01:23:45 EDT")
-            (:values (enc 45 23 1 2 3 2013 -4) 0))
-       (=>? (rfc822 "Sat, 02 Mar 2013 01:23:45 FOOBAZBAR")
-            (:values (enc 45 23 1 2 3 2013 0) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00 JST")
-            (:values (enc 0 0 0 1 1 2013 -9) 0))
-       (=>? (rfc822 "Sat, 01 Feb 2013 01:23:45 JST")
-            (:values (enc 45 23 1 1 2 2013 -9) 0))
-       (=>? (rfc822 "01 Dec 13 00:00 JST")
-            (:values (enc 0 0 0 1 12 2013 -9) 0))
-
-       (=>? (rfc822 "24 Dec 49 12:00 EST")
-            (:values (enc 0 0 12 24 12 2049 -5) 0))
-       (=>? (rfc822 "24 Dec 50 12:00 EST")
-            (:values (enc 0 0 12 24 12 1950 -5) 0))
-
-       (=>? (rfc822 "Sat, 01 Jan 2000 00:00:00.42 GMT")
-            (:values (enc 0 0 0 1 1 2000 0) 0.42))
-       (=>? (rfc822 "Sat, 01 Jan 2000 00:00:00:42 GMT")
-            (:values (enc 0 0 0 1 1 2000 0) 0.42))
-
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00 +0700")
-            (:values (enc 0 0 0 1 1 2013 -7) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00+1300")
-            (:values (enc 0 0 0 1 1 2013 -13) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00 +07:00")
-            (:values (enc 0 0 0 1 1 2013 -7) 0))
-       (=>? (rfc822 "Sat, 01 Jan 2013 00:00:00+13:00")
-            (:values (enc 0 0 0 1 1 2013 -13) 0))
-       (=>? (rfc822 "Sat, 01 Mar 2008 19:42:34 -0500")
-            (:values (enc 34 42 19 1 3 2008 5) 0))
-       (=>? (rfc822 "Sat, 01 Mar 2008 19:42:34-0500")
-            (:values (enc 34 42 19 1 3 2008 5) 0))
-       (=>? (rfc822 "Sat, 01 Mar 2008 19:42:34 -05:00")
-            (:values (enc 34 42 19 1 3 2008 5) 0))
-       (=>? (rfc822 "Sat, 01 Mar 2008 19:42:34-05:00")
-            (:values (enc 34 42 19 1 3 2008 5) 0))
-
-       (=>? (rfc822 "Thu, 01 Jan 2004")
-            (:values (enc 0 0 0 1 1 2004 0) 0))
-       (=>? (rfc822 "01 Jan 2004")
-            (:values (enc 0 0 0 1 1 2004 0) 0))
-       (=>? (rfc822 "1 Jan 04")
-            (:values (enc 0 0 0 1 1 2004 0) 0))
-
-       (=>? (rfc822 "Sun Jan 4 16:29:06 PST 2004")
-            (:values (enc 6 29 16 4 1 2004 -8) 0))
-
-       (=>? (rfc822 "Thu Jul 23 19:42:23 JST 2013")
-            (:values (enc 23 42 19 23 7 2013 -9) 0))
-
-       (=>? (rfc822 "Thudesday, 23-Jul-13 19:42:23 GMT")
-            (:values (enc 23 42 19 23 7 2013 0) 0))
-
-       ;; supplemental this year
-       (let ((this-year (nth-value 5 (decode-universal-time
-                                      (get-universal-time) 0))))
-         (=>? (rfc822 "Thu, 01 Jan")
-              (:values (enc 0 0 0 1 1 this-year 0) 0))
-         (=>? (rfc822 "01 Jan")
-              (:values (enc 0 0 0 1 1 this-year 0) 0))
-         (=>? (rfc822 "1 Jan")
-              (:values (enc 0 0 0 1 1 this-year 0) 0)))
-
-       )
 
 
 (defun parse-iso8601-genus (date-time-string)
@@ -655,82 +489,6 @@ Reference:
     (values universal-time fraction)))
 
 
-#+et (flet ((iso8601 (x) (parse-iso8601-genus x))
-            (enc (&rest args) (apply #'encode-universal-time args)))
-
-       (=>? (iso8601 "2000-01-01T00:00:00Z")
-            (:values (enc 0 0 0 1 1 2000 0) 0))
-
-       (=>? (iso8601 "2003-12-31T10:14:55.7-08:00")
-            (:values (enc 55 14 10 31 12 2003 +8) 0.7))
-       (=>? (iso8601 "2003-12-31T10:14:55.7-0800")
-            (:values (enc 55 14 10 31 12 2003 +8) 0.7))
-
-       (=>? (iso8601 "2003-12-31T10:14:55+08:00")
-            (:values (enc 55 14 10 31 12 2003 -8) 0))
-       (=>? (iso8601 "2003-12-31T10:14:55+0800")
-            (:values (enc 55 14 10 31 12 2003 -8) 0))
-
-       (=>? (iso8601 "2003-12-31T10:14:55.7Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0.7))
-       (=>? (iso8601 "2003-12-31T10:14:55Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0))
-
-       (=>? (iso8601 "2003-12-31T10:14Z")
-            (:values (enc 0 14 10 31 12 2003 0) 0))
-
-       (=>? (iso8601 "2003-12-31T101455-08:00")
-            (:values (enc 55 14 10 31 12 2003 +8) 0))
-       (=>? (iso8601 "2003-12-31T101455-0800")
-            (:values (enc 55 14 10 31 12 2003 +8) 0))
-       (=>? (iso8601 "2003-12-31T1014-08:00")
-            (:values (enc 0 14 10 31 12 2003 +8) 0))
-       (=>? (iso8601 "2003-12-31T1014-0800")
-            (:values (enc 0 14 10 31 12 2003 +8) 0))
-       (=>? (iso8601 "2003-12-31T1014557Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0.7))
-       (=>? (iso8601 "2003-12-31T101455Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0))
-       (=>? (iso8601 "2003-12-31T1014Z")
-            (:values (enc 0 14 10 31 12 2003 0) 0))
-       (=>? (iso8601 "2003-12-31T10Z")
-            (:values (enc 0 0 10 31 12 2003 0) 0))
-
-       (=>? (iso8601 "2003")
-            (:values (enc 0 0 0 1 1 2003 0) 0))
-       (=>? (iso8601 "2003-12")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (iso8601 "2003-12-31")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-       (=>? (iso8601 "20031231")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-
-       (=>? (iso8601 "-03-12")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (iso8601 "-03")
-            (:values (enc 0 0 0 1 1 2003 0) 0))
-       (=>? (iso8601 "-0312")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (iso8601 "-03-12-31")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-
-       (=>? (iso8601 "03-12-31")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-
-       (=>? (iso8601 "031231")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-
-       (=>? (iso8601 "2003335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (iso8601 "2003-335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (iso8601 "03335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (iso8601 "03-335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       )
-
-
 (defun parse-date-time (date-time-string)
   "Parse DATE-TIME-STRING, and return (values UNIVERSAL-TIME FRACTION).
 DATE-TIME-STRING must represent the date-time after 1900-01-01T00:00:00Z.
@@ -762,180 +520,6 @@ Examples:
   (if (ppcre:scan "[a-zA-Z]{2}" date-time-string)
       (parse-rfc822-genus date-time-string)
       (parse-iso8601-genus date-time-string)))
-
-
-#+et (flet ((parse (x) (parse-date-time x))
-            (enc (&rest args) (apply #'encode-universal-time args)))
-
-       ;; rfc822-genus
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00 GMT")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00 gmt")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00 Z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00Z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00 z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00z")
-            (:values (enc 0 0 0 1 1 2013 0) 0))
-
-       (=>? (parse "Sat, 02 Mar 2013 01:23:45 EDT")
-            (:values (enc 45 23 1 2 3 2013 -4) 0))
-       (=>? (parse "Sat, 02 Mar 2013 01:23:45 FOOBAZBAR")
-            (:values (enc 45 23 1 2 3 2013 0) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00 JST")
-            (:values (enc 0 0 0 1 1 2013 -9) 0))
-       (=>? (parse "Sat, 01 Feb 2013 01:23:45 JST")
-            (:values (enc 45 23 1 1 2 2013 -9) 0))
-       (=>? (parse "01 Dec 13 00:00 JST")
-            (:values (enc 0 0 0 1 12 2013 -9) 0))
-
-       (=>? (parse "24 Dec 49 12:00 EST")
-            (:values (enc 0 0 12 24 12 2049 -5) 0))
-       (=>? (parse "24 Dec 50 12:00 EST")
-            (:values (enc 0 0 12 24 12 1950 -5) 0))
-
-       (=>? (parse "Sat, 01 Jan 2000 00:00:00.42 GMT")
-            (:values (enc 0 0 0 1 1 2000 0) 0.42))
-       (=>? (parse "Sat, 01 Jan 2000 00:00:00:42 GMT")
-            (:values (enc 0 0 0 1 1 2000 0) 0.42))
-
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00 +0700")
-            (:values (enc 0 0 0 1 1 2013 -7) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00+1300")
-            (:values (enc 0 0 0 1 1 2013 -13) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00 +07:00")
-            (:values (enc 0 0 0 1 1 2013 -7) 0))
-       (=>? (parse "Sat, 01 Jan 2013 00:00:00+13:00")
-            (:values (enc 0 0 0 1 1 2013 -13) 0))
-
-       (=>? (parse "Thu, 01 Jan 2004")
-            (:values (enc 0 0 0 1 1 2004 0) 0))
-       (=>? (parse "01 Jan 2004")
-            (:values (enc 0 0 0 1 1 2004 0) 0))
-       (=>? (parse "1 Jan 04")
-            (:values (enc 0 0 0 1 1 2004 0) 0))
-
-       (=>? (parse "Sun Jan 4 16:29:06 PST 2004")
-            (:values (enc 6 29 16 4 1 2004 -8) 0))
-
-       ;; iso8601-genus
-       (=>? (parse "2000-01-01T00:00:00Z")
-            (:values (enc 0 0 0 1 1 2000 0) 0))
-
-       (=>? (parse "2003-12-31T10:14:55.7-08:00")
-            (:values (enc 55 14 10 31 12 2003 +8) 0.7))
-       (=>? (parse "2003-12-31T10:14:55.7-0800")
-            (:values (enc 55 14 10 31 12 2003 +8) 0.7))
-
-       (=>? (parse "2003-12-31T10:14:55+08:00")
-            (:values (enc 55 14 10 31 12 2003 -8) 0))
-       (=>? (parse "2003-12-31T10:14:55+0800")
-            (:values (enc 55 14 10 31 12 2003 -8) 0))
-
-       (=>? (parse "2003-12-31T10:14:55.7Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0.7))
-       (=>? (parse "2003-12-31T10:14:55Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0))
-
-       (=>? (parse "2003-12-31T10:14Z")
-            (:values (enc 0 14 10 31 12 2003 0) 0))
-
-       (=>? (parse "2003-12-31T101455-08:00")
-            (:values (enc 55 14 10 31 12 2003 +8) 0))
-       (=>? (parse "2003-12-31T101455-0800")
-            (:values (enc 55 14 10 31 12 2003 +8) 0))
-       (=>? (parse "2003-12-31T1014-08:00")
-            (:values (enc 0 14 10 31 12 2003 +8) 0))
-       (=>? (parse "2003-12-31T1014-0800")
-            (:values (enc 0 14 10 31 12 2003 +8) 0))
-       (=>? (parse "2003-12-31T1014557Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0.7))
-       (=>? (parse "2003-12-31T101455Z")
-            (:values (enc 55 14 10 31 12 2003 0) 0))
-       (=>? (parse "2003-12-31T1014Z")
-            (:values (enc 0 14 10 31 12 2003 0) 0))
-       (=>? (parse "2003-12-31T10Z")
-            (:values (enc 0 0 10 31 12 2003 0) 0))
-
-       (=>? (parse "2003")
-            (:values (enc 0 0 0 1 1 2003 0) 0))
-       (=>? (parse "2003-12")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (parse "2003-12-31")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-       (=>? (parse "20031231")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-
-       (=>? (parse "-03-12")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (parse "-03")
-            (:values (enc 0 0 0 1 1 2003 0) 0))
-       (=>? (parse "-0312")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (parse "-03-12-31")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-       
-       (=>? (parse "03-12-31")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-
-       (=>? (parse "031231")
-            (:values (enc 0 0 0 31 12 2003 0) 0))
-
-       (=>? (parse "2003335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (parse "2003-335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (parse "03335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-       (=>? (parse "03-335")
-            (:values (enc 0 0 0 1 12 2003 0) 0))
-
-       ;; c.f. RFC 3339, 3. Two Digit Years, last item
-       (=>? (parse ":0-09-09")
-            (:values (enc 0 0 0 9 9 2000 0) 0))
-       (=>? (parse ";0-09-09")
-            (:values (enc 0 0 0 9 9 2010 0) 0))
-       
-       ;; supplemental this year
-       (let ((this-year (nth-value 5 (decode-universal-time
-                                      (get-universal-time) 0))))
-         (=>? (parse "Thu, 01 Jan")
-              (:values (enc 0 0 0 1 1 this-year 0) 0))
-         (=>? (parse "01 Jan")
-              (:values (enc 0 0 0 1 1 this-year 0) 0))
-         (=>? (parse "1 Jan")
-              (:values (enc 0 0 0 1 1 this-year 0) 0)))
-
-       ;; bogus W3CDTF (invalid hour)
-       (=>? (parse "2003-12-31T25:14:55Z")
-            (:values (enc 55 14 1 1 1 2004 0) 0))
-
-       ;; bogus W3CDTF (invalid minute)
-       (=>? (parse "2003-12-31T10:61:55Z")
-            (:values (enc 55 1 11 31 12 2003 0) 0))
-
-       ;; bogus W3CDTF (invalid second)
-       (=>? (parse "2003-12-31T10:14:61Z")
-            (:values (enc 1 15 10 31 12 2003 0) 0))
-
-       ;; MSSQL date time format
-       (=>? (parse "2004-07-08 23:56:58.7")
-            (:values (enc 58 56 23 8 7 2004 0) 0.7))
-
-       ;; MSSQL-ish date time format (without fractional second)
-       (=>? (parse "2004-07-08 23:56:58")
-            (:values (enc 58 56 23 8 7 2004 0) 0))
-
-       ;; above some examples from http://pythonhosted.org/feedparser/date-parsing.html
-       )
-
-
-;; Cleanup for Eval-Test
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (setf *features* *features-tmp*))
 
 
 ;;====================================================================
